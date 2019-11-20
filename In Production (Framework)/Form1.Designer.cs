e7f85f8d6769afeb82b3838a7c12bdcbd9697ef5
DatJeeves <57726688+DatJeeves@users.noreply.github.com>
2019-11-20 10:43:33 -0800

    New Design
    New Design

---
+++ In Production (Framework)/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace In_Production__Framework_
+{
+    partial class InitialScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProductionTitleTextBox = new System.Windows.Forms.TextBox();
+            this.DirectorInput = new System.Windows.Forms.TextBox();
+            this.DirectorRole = new System.Windows.Forms.RichTextBox();
+            this.ProducerInput = new System.Windows.Forms.TextBox();
+            this.ProducerRole = new System.Windows.Forms.RichTextBox();
+            this.FirstADInput = new System.Windows.Forms.TextBox();
+            this.FirstADRole = new System.Windows.Forms.RichTextBox();
+            this.CinematographerInput = new System.Windows.Forms.TextBox();
+            this.CinematographerRole = new System.Windows.Forms.RichTextBox();
+            this.LocationInput = new System.Windows.Forms.TextBox();
+            this.LocationText = new System.Windows.Forms.RichTextBox();
+            this.CallTimeInput = new System.Windows.Forms.TextBox();
+            this.CallTimeText = new System.Windows.Forms.RichTextBox();
+            this.ShootingTimeInput = new System.Windows.Forms.TextBox();
+            this.ShootingTimeText = new System.Windows.Forms.RichTextBox();
+            this.DateTextBox = new System.Windows.Forms.RichTextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ProductionTitleTextBox
+            // 
+            this.ProductionTitleTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProductionTitleTextBox.Location = new System.Drawing.Point(1281, 85);
+            this.ProductionTitleTextBox.Margin = new System.Windows.Forms.Padding(6);
+            this.ProductionTitleTextBox.Name = "ProductionTitleTextBox";
+            this.ProductionTitleTextBox.Size = new System.Drawing.Size(627, 98);
+            this.ProductionTitleTextBox.TabIndex = 1;
+            // 
+            // DirectorInput
+            // 
+            this.DirectorInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DirectorInput.Location = new System.Drawing.Point(545, 882);
+            this.DirectorInput.Margin = new System.Windows.Forms.Padding(6);
+            this.DirectorInput.Name = "DirectorInput";
+            this.DirectorInput.Size = new System.Drawing.Size(521, 62);
+            this.DirectorInput.TabIndex = 3;
+            // 
+            // DirectorRole
+            // 
+            this.DirectorRole.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DirectorRole.Location = new System.Drawing.Point(63, 882);
+            this.DirectorRole.Margin = new System.Windows.Forms.Padding(6);
+            this.DirectorRole.Name = "DirectorRole";
+            this.DirectorRole.Size = new System.Drawing.Size(464, 84);
+            this.DirectorRole.TabIndex = 2;
+            this.DirectorRole.Text = "Director:";
+            // 
+            // ProducerInput
+            // 
+            this.ProducerInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProducerInput.Location = new System.Drawing.Point(2101, 894);
+            this.ProducerInput.Margin = new System.Windows.Forms.Padding(6);
+            this.ProducerInput.Name = "ProducerInput";
+            this.ProducerInput.Size = new System.Drawing.Size(521, 62);
+            this.ProducerInput.TabIndex = 5;
+            // 
+            // ProducerRole
+            // 
+            this.ProducerRole.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProducerRole.Location = new System.Drawing.Point(1775, 882);
+            this.ProducerRole.Margin = new System.Windows.Forms.Padding(6);
+            this.ProducerRole.Name = "ProducerRole";
+            this.ProducerRole.Size = new System.Drawing.Size(285, 84);
+            this.ProducerRole.TabIndex = 4;
+            this.ProducerRole.Text = "Producer:";
+            // 
+            // FirstADInput
+            // 
+            this.FirstADInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FirstADInput.Location = new System.Drawing.Point(2101, 1042);
+            this.FirstADInput.Margin = new System.Windows.Forms.Padding(6);
+            this.FirstADInput.Name = "FirstADInput";
+            this.FirstADInput.Size = new System.Drawing.Size(521, 62);
+            this.FirstADInput.TabIndex = 7;
+            // 
+            // FirstADRole
+            // 
+            this.FirstADRole.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FirstADRole.Location = new System.Drawing.Point(1775, 1030);
+            this.FirstADRole.Margin = new System.Windows.Forms.Padding(6);
+            this.FirstADRole.Name = "FirstADRole";
+            this.FirstADRole.Size = new System.Drawing.Size(285, 84);
+            this.FirstADRole.TabIndex = 6;
+            this.FirstADRole.Text = "1st AD";
+            // 
+            // CinematographerInput
+            // 
+            this.CinematographerInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CinematographerInput.Location = new System.Drawing.Point(545, 1042);
+            this.CinematographerInput.Margin = new System.Windows.Forms.Padding(6);
+            this.CinematographerInput.Name = "CinematographerInput";
+            this.CinematographerInput.Size = new System.Drawing.Size(521, 62);
+            this.CinematographerInput.TabIndex = 9;
+            // 
+            // CinematographerRole
+            // 
+            this.CinematographerRole.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CinematographerRole.Location = new System.Drawing.Point(63, 1030);
+            this.CinematographerRole.Margin = new System.Windows.Forms.Padding(6);
+            this.CinematographerRole.Name = "CinematographerRole";
+            this.CinematographerRole.Size = new System.Drawing.Size(464, 84);
+            this.CinematographerRole.TabIndex = 8;
+            this.CinematographerRole.Text = "Cinematographer:";
+            // 
+            // LocationInput
+            // 
+            this.LocationInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LocationInput.Location = new System.Drawing.Point(1317, 1299);
+            this.LocationInput.Margin = new System.Windows.Forms.Padding(6);
+            this.LocationInput.Name = "LocationInput";
+            this.LocationInput.Size = new System.Drawing.Size(627, 75);
+            this.LocationInput.TabIndex = 11;
+            // 
+            // LocationText
+            // 
+            this.LocationText.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LocationText.Location = new System.Drawing.Point(735, 1299);
+            this.LocationText.Margin = new System.Windows.Forms.Padding(6);
+            this.LocationText.Name = "LocationText";
+            this.LocationText.Size = new System.Drawing.Size(538, 84);
+            this.LocationText.TabIndex = 10;
+            this.LocationText.Text = "Location:";
+            // 
+            // CallTimeInput
+            // 
+            this.CallTimeInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CallTimeInput.ForeColor = System.Drawing.Color.Red;
+            this.CallTimeInput.Location = new System.Drawing.Point(1281, 500);
+            this.CallTimeInput.Margin = new System.Windows.Forms.Padding(6);
+            this.CallTimeInput.Name = "CallTimeInput";
+            this.CallTimeInput.Size = new System.Drawing.Size(627, 75);
+            this.CallTimeInput.TabIndex = 13;
+            // 
+            // CallTimeText
+            // 
+            this.CallTimeText.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CallTimeText.ForeColor = System.Drawing.Color.Red;
+            this.CallTimeText.Location = new System.Drawing.Point(574, 500);
+            this.CallTimeText.Margin = new System.Windows.Forms.Padding(6);
+            this.CallTimeText.Name = "CallTimeText";
+            this.CallTimeText.Size = new System.Drawing.Size(663, 80);
+            this.CallTimeText.TabIndex = 12;
+            this.CallTimeText.Text = "CALL TIME:";
+            // 
+            // ShootingTimeInput
+            // 
+            this.ShootingTimeInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ShootingTimeInput.Location = new System.Drawing.Point(1281, 648);
+            this.ShootingTimeInput.Margin = new System.Windows.Forms.Padding(6);
+            this.ShootingTimeInput.Name = "ShootingTimeInput";
+            this.ShootingTimeInput.Size = new System.Drawing.Size(627, 75);
+            this.ShootingTimeInput.TabIndex = 15;
+            // 
+            // ShootingTimeText
+            // 
+            this.ShootingTimeText.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ShootingTimeText.Location = new System.Drawing.Point(574, 648);
+            this.ShootingTimeText.Margin = new System.Windows.Forms.Padding(6);
+            this.ShootingTimeText.Name = "ShootingTimeText";
+            this.ShootingTimeText.Size = new System.Drawing.Size(663, 80);
+            this.ShootingTimeText.TabIndex = 14;
+            this.ShootingTimeText.Text = "SHOOTING TIME:";
+            // 
+            // DateTextBox
+            // 
+            this.DateTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DateTextBox.Location = new System.Drawing.Point(783, 313);
+            this.DateTextBox.Margin = new System.Windows.Forms.Padding(6);
+            this.DateTextBox.Name = "DateTextBox";
+            this.DateTextBox.Size = new System.Drawing.Size(399, 97);
+            this.DateTextBox.TabIndex = 16;
+            this.DateTextBox.Text = "DATE:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(1246, 313);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(6);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(912, 62);
+            this.dateTimePicker1.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(627, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(610, 91);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Production Title:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // InitialScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2704, 1717);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.DateTextBox);
+            this.Controls.Add(this.ShootingTimeInput);
+            this.Controls.Add(this.ShootingTimeText);
+            this.Controls.Add(this.CallTimeInput);
+            this.Controls.Add(this.CallTimeText);
+            this.Controls.Add(this.LocationInput);
+            this.Controls.Add(this.LocationText);
+            this.Controls.Add(this.CinematographerInput);
+            this.Controls.Add(this.CinematographerRole);
+            this.Controls.Add(this.FirstADInput);
+            this.Controls.Add(this.FirstADRole);
+            this.Controls.Add(this.ProducerInput);
+            this.Controls.Add(this.ProducerRole);
+            this.Controls.Add(this.DirectorInput);
+            this.Controls.Add(this.DirectorRole);
+            this.Controls.Add(this.ProductionTitleTextBox);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "InitialScreen";
+            this.Text = "InitialScreen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox ProductionTitleTextBox;
+        private System.Windows.Forms.TextBox DirectorInput;
+        private System.Windows.Forms.RichTextBox DirectorRole;
+        private System.Windows.Forms.TextBox ProducerInput;
+        private System.Windows.Forms.RichTextBox ProducerRole;
+        private System.Windows.Forms.TextBox FirstADInput;
+        private System.Windows.Forms.RichTextBox FirstADRole;
+        private System.Windows.Forms.TextBox CinematographerInput;
+        private System.Windows.Forms.RichTextBox CinematographerRole;
+        private System.Windows.Forms.TextBox LocationInput;
+        private System.Windows.Forms.RichTextBox LocationText;
+        private System.Windows.Forms.TextBox CallTimeInput;
+        private System.Windows.Forms.RichTextBox CallTimeText;
+        private System.Windows.Forms.TextBox ShootingTimeInput;
+        private System.Windows.Forms.RichTextBox ShootingTimeText;
+        private System.Windows.Forms.RichTextBox DateTextBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
